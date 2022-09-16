d7d32ec4bb33203f9adb7dcf87866954c4a45189
ytang <107004276+yota88@users.noreply.github.com>
2022-09-16 14:37:35 -0400

    Removed few module imports

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const path = require('path');
 const router = require('./routes');
-const db = require('../db');
 
 const app = express();
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const controller =  require('../controllers');
 const router = express.Router();
+const controller =  require('../controllers');
 
 router.get('/reviews', controller.getReviews);
 
